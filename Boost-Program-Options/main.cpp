5d9ba33f355889b3ae82eb3c20536970b8f667b3
lopezfjose <jflopezfernandez@gmail.com>
2018-05-04 14:50:08 -0400

    Successfully configured project

--- Boost-Program-Options/main.cpp
+++ Boost-Program-Options/main.cpp
@@ -1,8 +1,20 @@
 
+#include <boost/program_options.hpp>
 #include <iostream>
 
+namespace Experimental {
+	template <typename T>
+	T* addressOf(T& arg) {
+		return &arg;
+	}
+}
 
-int main()
+
+int main(int argc, char *argv[])
 {
+	boost::program_options::options_description generic("Generic options");
+
 	std::cout << "testing...\n";
+
+	return EXIT_SUCCESS;
 }
